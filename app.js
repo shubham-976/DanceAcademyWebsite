9bca6ee2371e10cd6755f4c3c276a18968e237b1
shubham-976 <97439744+shubham-976@users.noreply.github.com>
2022-07-10 09:54:42 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const app = express();
+
+//for static files
+app.use('/static', express.static('static'))
+
+//for storing user filled data, if any
+app.use(express.urlencoded())
+
+//for template engine
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'template'))
+
+//end points
+app.get("/", (req, res)=>{
+    res.status(200).render('home.pug', {}); //{} mean no parameter passed.
+})
+app.get("/contact", (req, res)=>{
+    res.status(200).render('contact.pug', {}); //{} mean no parameter passed.
+})
+
+//start the server
+app.listen(port, ()=>{
+    console.log(`Server is running at http://${hostname}:${port}/`)
+})
